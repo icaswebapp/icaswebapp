fc3e9a9bf93fe9121c2db88047dd50defe45b4f6
pd-escher <dhoolia.pranav@gmail.com>
2019-06-19 13:12:17 +0530

    Implemented bare minimum forums

--- src/Components/Forums/Forums.js
+++ src/Components/Forums/Forums.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import protectedScreen from '../../Backend/Protector';
-
+import firebase from '../../Backend/Firebase';
+var ForumsItem = require('./ForumsItem').default
 
 const Forums = (props) => {
-    
+    const [forumsCards, setforumsCards] = useState(null);
+
+    // get all the documents in the collection
+    async function getCollection() {
+        const markers = [];
+        await firebase.getFirestore().collection('Posts').get()
+            .then(querySnapshot => {
+                querySnapshot.docs.forEach(doc => {
+                    markers.push(doc.data());
+                });
+            });
+        return markers;
+    }
+
+    getCollection().then(function (collection) {
+        setforumsCards(
+            collection.map((forumObject, index) => {
+                return <ForumsItem 
+                    key={index} 
+                    object={forumObject} 
+                    position={index}
+                />
+            })
+        )
+
+        console.log(collection)
+        collection.forEach(doc => {
+            return doc.map
+        })
+    }).catch(function (error) {
+        console.log("Error getting collection:", error);
+    });
+
     return (
         <div>
-            <h1 className='f1 tc fw9'>FORUMS</h1>
+            <h1 className="f1 tc black underline fw9">FORUMS</h1>
+            {forumsCards}
         </div>
     )
 }
